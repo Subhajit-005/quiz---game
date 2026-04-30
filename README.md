b302caea24190fdf2b0c17938ac3992a4b43d9f3
Subhajit-005 <bikram98pal@gmail.com>
2026-04-30 11:09:55 +0530

    Initial commit - Smart Quiz Game with timer and score system
    Smart Quiz Game built in Python with timer, difficulty levels, random questions, and score tracking.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# quiz---game
-A Python-based quiz game that asks multiple-choice questions and calculates the user’s score using basic programming concepts.
+# Smart Quiz Game 🎮
+
+## Description
+
+A Python-based quiz game with timer, difficulty levels, and score tracking.
+
+## Features
+
+* Multiple choice questions
+* Timer-based answering
+* Random question selection
+* Difficulty levels
+* Score saving system
+
+## How to Run
+
+1. Install Python
+2. Run:
+   python main.py
+
+## Project Structure
+
+* main.py → Main program
+* quiz_logic.py → Quiz logic
+* questions.py → Question database
+* utils.py → Timer & score saving
+
+## Future Improvements
+
+* GUI version
+* Leaderboard system
+* Online quiz support
---
+++ quiz_logic.py
@@ -0,0 +1,31 @@
+from utils import timer_input
+
+def ask_question(q):
+    print(q["question"])
+
+    for i, option in enumerate(q["options"], start=1):
+        print(f"{i}. {option}")
+
+    answer = timer_input(10)
+
+    if answer is None:
+        print(f"⏰ Time up! Correct answer is: {q['answer']}")
+        return False
+
+    try:
+        answer = int(answer)
+
+        if answer < 1 or answer > len(q["options"]):
+            print(f"❌ Invalid choice! Correct answer is: {q['answer']}")
+            return False
+
+        if q["options"][answer - 1] == q["answer"]:
+            print("✅ Correct!")
+            return True
+        else:
+            print(f"❌ Wrong! Correct answer is: {q['answer']}")
+            return False
+
+    except:
+        print(f"❌ Invalid input! Correct answer is: {q['answer']}")
+        return False
